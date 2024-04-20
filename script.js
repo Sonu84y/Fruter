36c854f1978ee6cc5ffe0ab315da0febb9630ee2
Sonu Chaurasiya <155141030+Sonu84y@users.noreply.github.com>
2024-04-20 20:38:53 +0530

    Secoand change

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 // Search box functions
 
 
-let src=document.querySelector("input");
+let src=document.querySelector(".input");
+let li= document.querySelector("#ho");
+
 
 src.onclick=()=>{
     src.style.border="2px solid aqua";
-    src.style.backgroundColor="gray";
+}
+li.onclick=()=>{
+    src.style.border="2px solid transparent";
 }
 
 
-let body=document.querySelector(".body_section");
+let body=document.querySelector(".section");
 
 body.onclick=()=>{
    src.style.border="2px solid transparent";
